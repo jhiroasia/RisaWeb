814c4b4eeaa88715731f4c8d53dd470189a7cd41
Jhiro <jhiroasia@gmail.com>
2025-10-19 15:02:53 +0700

    made a cool button and search filter

---
+++ button.js
@@ -0,0 +1,18 @@
+<script>
+  const btn = document.getElementById("backToTop");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  btn.onclick = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+</script>
